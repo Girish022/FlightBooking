3b5fe1232dacf1aa8485d320dd07155da5a9b2bd
Manohar R <manohar.r@honeywell.com>
2020-05-04 23:06:41 +0530

    Flight Booking 2nd draft

--- src/app/user/print/print.component.ts
+++ src/app/user/print/print.component.ts
@@ -8,20 +8,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./print.component.css']
 })
 export class PrintComponent implements OnInit {
-createTicket;
+  createTicket;
   constructor(
-    private BookingService:BookingService,
-    private route:Router
+    private BookingService: BookingService,
+    private route: Router
   ) { }
 
   ngOnInit() {
-  
-    this.BookingService.cast.subscribe(
-      res=>this.createTicket=res,
-    )
-    if(!this.createTicket){
-      this.route.navigate([''])
+    let journey = JSON.parse(localStorage.getItem("journey"))
+    let journeyDetails = JSON.parse(localStorage.getItem("journeyDetails"));
+
+    if (journeyDetails && journey) {
+      for (let i = 0; i < journeyDetails.length; i++) {
+        let existingJourneyBusId = journeyDetails[i].journey.bus['$key'];
+        let existingJourneyDate = journeyDetails[i].journey.journey_route.date;
+        let existingJourneyTime = journeyDetails[i].journey.bus.time;
+        let existingJourneySeats = journeyDetails[i].journey.seats;
+
+        if (existingJourneyBusId === journey.bus['$key'] && existingJourneyDate === journey.journey_route.date
+          && existingJourneyTime === journey.bus.time && existingJourneySeats.includes(journey.seats[0])) {          
+          this.createTicket = journeyDetails[i];
+          break;
+        }
+      }
     }
+
+    //this.BookingService.cast.subscribe(
+    //  res=>this.createTicket=res,
+    //)
+    //if(!this.createTicket){
+    //  this.route.navigate([''])
+    //}
   }
 
 }
