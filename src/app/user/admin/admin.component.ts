26174c0caa2108a25f10e5361fce5eb8cccb0b5c
Manohar R <manohar.r@honeywell.com>
2020-05-05 13:09:44 +0530

    Display booking on flight select

--- src/app/user/admin/admin.component.ts
+++ src/app/user/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,26 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  journeyDetails;
   flights = [];
-  displayedColumns: string[] = ['user', 'seat'];
   selectedFlight;
-  constructor() { }
+  bookingList = [];
+  modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
-    let journeyDetails = JSON.parse(localStorage.getItem("journeyDetails"));
-    if (journeyDetails) {
-      for (let i = 0; i < journeyDetails.length; i++) {
+    this.journeyDetails = JSON.parse(localStorage.getItem("journeyDetails"));
+    if (this.journeyDetails) {
+      for (let i = 0; i < this.journeyDetails.length; i++) {
         let found = false;
         for (let j = 0; j < this.flights.length; j++) {
-          if (this.flights[j].key === journeyDetails[i].journey.bus.$key) {
+          if (this.flights[j].key === this.journeyDetails[i].journey.bus.$key) {
             found = true;
             break;
           }
         }
         if (!found) {
           this.flights.push({
-            key: journeyDetails[i].journey.bus.$key,
-            name: journeyDetails[i].journey.bus.location + ' ' + journeyDetails[i].journey.bus.time
+            key: this.journeyDetails[i].journey.bus.$key,
+            name: this.journeyDetails[i].journey.bus.location + ' ' + this.journeyDetails[i].journey.bus.time
           })
         }        
       }
@@ -34,7 +39,37 @@ export class AdminComponent implements OnInit {
   }
 
   onFlightSelect() {
-    console.log('Flight selected' + this.selectedFlight)
+    console.log('Flight selected' + this.selectedFlight);
+    this.bookingList = [];
+    for (let i = 0; i < this.journeyDetails.length; i++) {
+      if (this.journeyDetails[i].journey.bus.$key === this.selectedFlight) {
+        this.bookingList.push(this.journeyDetails[i]);
+      }
+    }
   }
 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  cancelBooking(booking) {
+    console.log('Flight selected' + booking)
+    for (let i = 0; i < this.journeyDetails.length; i++) {
+      if (this.journeyDetails[i].journey.bus.$key === booking.journey.bus.$key &&
+        this.journeyDetails[i].journey.journey_route.date === booking.journey.journey_route.date &&
+        this.journeyDetails[i].journey.bus.time === booking.journey.bus.time &&
+        this.journeyDetails[i].journey.seats === booking.journey.seats) {
+
+        this.journeyDetails.splice(i, 1);
+        break;
+      }
+    }
+    this.onFlightSelect();
+    localStorage.setItem("journeyDetails", JSON.stringify(this.journeyDetails));
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.modalRef.hide();
+  }
 }
